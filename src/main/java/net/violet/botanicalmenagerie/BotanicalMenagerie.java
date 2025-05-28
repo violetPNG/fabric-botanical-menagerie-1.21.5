26e089f6b3f640631733bc0e138a5657341bea60
itsDraxus <liam@cyphers.ca>
2025-05-27 23:43:42 -0400

    Added Golem Core

--- src/main/java/net/violet/botanicalmenagerie/BotanicalMenagerie.java
+++ src/main/java/net/violet/botanicalmenagerie/BotanicalMenagerie.java
@@ -2,6 +2,7 @@ package net.violet.botanicalmenagerie;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.violet.botanicalmenagerie.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,6 @@ public class BotanicalMenagerie implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
--- src/main/java/net/violet/botanicalmenagerie/item/ModItems.java
+++ src/main/java/net/violet/botanicalmenagerie/item/ModItems.java
@@ -1,4 +1,29 @@
 package net.violet.botanicalmenagerie.item;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+import net.violet.botanicalmenagerie.BotanicalMenagerie;
+
+
 public class ModItems {
+
+    public static final Item GOLEM_CORE = registerItem("golem_core", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(BotanicalMenagerie.MOD_ID,"golem_core")))));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(BotanicalMenagerie.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        BotanicalMenagerie.LOGGER.info("Registering Mod Items for " + BotanicalMenagerie.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(GOLEM_CORE);
+        });
+    }
 }
